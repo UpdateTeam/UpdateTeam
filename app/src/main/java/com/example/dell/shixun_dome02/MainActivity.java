a410dd0546b6624a7bd3cac87cdc6402369e0513
dome1 <1035630950@qq.com>
2018-05-10 20:57:33 +0800

    sdas

--- app/src/main/java/com/example/dell/shixun_dome02/MainActivity.java
+++ app/src/main/java/com/example/dell/shixun_dome02/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dell.shixun_dome02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void getbut(View view) {
         Toast.makeText(MainActivity.this,"你真帅",Toast.LENGTH_LONG).show();
+        Log.d("123","asd");
     }
 }
